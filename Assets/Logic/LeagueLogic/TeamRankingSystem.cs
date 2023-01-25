fbe26f1c63f97f427f562d8dff8748f9d8731e79
AstraStudios <98630492+AstraStudios@users.noreply.github.com>
2023-01-24 23:22:05 -0600

    Update .1
    Added:
    -Background
    -Various sprites
    -Started working on league management scripts
    -Started on layers and UI
    -More small changes

---
+++ Assets/Logic/LeagueLogic/TeamRankingSystem.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TeamRankingSystem : MonoBehaviour
+{
+    public int teamWins;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
